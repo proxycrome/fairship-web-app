f8321909218c1863a82f08e95d0fa14fd0299626
victoreze22 <victor.ezeonwuka@optisoft.ng>
2022-03-29 21:45:22 +0100

    agent store modified

--- src/store/Agent/actions.js
+++ src/store/Agent/actions.js
@@ -1,27 +1,97 @@
 import {
-  FETCH_AGENT,
-  FETCH_AGENT_SUCCESSFUL,
-  FETCH_AGENT_ERROR,
+    GET_ALL_AGENTS,
+    GET_ALL_AGENTS_SUCCESSFUL,
+    GET_ALL_AGENTS_FAILURE,
+    POST_AGENT,
+    POST_AGENT_SUCCESSFUL,
+    POST_AGENT_FAILURE,
+    GET_LANDLORD_AGENTS,
+    GET_LANDLORD_AGENTS_SUCCESS,
+    GET_LANDLORD_AGENTS_FAILURE,
+    FETCH_AGENT,
+    FETCH_AGENT_SUCCESS,
+    FETCH_AGENT_FAILURE,
+} from './actionTypes'
 
-} from './actionTypes.js';
+export const getAgents = () => {
+    return {
+        type: GET_ALL_AGENTS,
+    }
+}
 
-// Fetch Agent
-export const fetchAgent = () => {
-  return {
-    type: FETCH_AGENT,
-  };
-};
+export const getAgentsSuccessful = (data) => {
+    return {
+        type: GET_ALL_AGENTS_SUCCESSFUL,
+        payload: data
+    }
+}
 
-export const fetchAgentSuccessful = (message) => {
-  return {
-    type: FETCH_AGENT_SUCCESSFUL,
-    payload: message,
-  };
-};
+export const getAgentsFailure = (error) => {
+    return {
+        type: GET_ALL_AGENTS_FAILURE,
+        payload: error
+    }
+}
 
-export const fetchAgentError = (error) => {
-  return {
-    type: FETCH_AGENT_ERROR,
-    payload: error,
-  };
-};
+export const postAgents = (formData) => {
+    return {
+        type: POST_AGENT,
+        payload: {formData}
+    }
+} 
+
+export const postAgentSuccessful = (data) => {
+    return {
+        type: POST_AGENT_SUCCESSFUL,
+        payload: data
+    }
+}
+
+export const postAgentFailure = (error) => {
+    return {
+        type: POST_AGENT_FAILURE,
+        payload: error
+    }
+}
+
+export const getLandlordAgents = (landlordId) => {
+    return {
+        type: GET_LANDLORD_AGENTS,
+        payload: {landlordId}
+    }
+}
+
+export const getLandlordAgentsSuccess = (data) => {
+    return {
+        type: GET_LANDLORD_AGENTS_SUCCESS,
+        payload: data
+    }
+}
+
+export const getLandlordAgentsFailure = (error) => {
+    return {
+        type: GET_LANDLORD_AGENTS_FAILURE,
+        payload : error
+    }
+}
+
+export const fetchAgent = (agentEmail) => {
+    return {
+        type: FETCH_AGENT,
+        payload: { agentEmail }
+    }
+}
+
+export const fetchAgentSuccess = (data) => {
+    return {
+        type: FETCH_AGENT_SUCCESS,
+        payload: data
+    }
+}
+
+export const fetchAgentFailure = (error) => {
+    return {
+        type: FETCH_AGENT_FAILURE,
+        payload: error
+    }
+}
