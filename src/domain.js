6b3da06b656a43f60f52c9d11fe335607bf04244
victoreze22 <victor.ezeonwuka@optisoft.ng>
2023-01-25 14:47:23 +0100

    production Url

--- src/domain.js
+++ src/domain.js
@@ -1,3 +1,3 @@
-const domain =  'http://134.209.64.28:8084/'  /*'https://api-backend.pmanager.online:8080/'*/;
+const domain =  /*'http://134.209.64.28:8084/'*/  'https://api-backend.pmanager.online:8080/';
 
 export default domain;
